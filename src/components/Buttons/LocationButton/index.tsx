f12ae681627d057873783c7b02da62d006603e0f
Elias Lima <sci.eliaslima@gmail.com>
2022-12-13 18:15:40 -0300

    Updated location button and logo

--- src/components/Buttons/LocationButton/index.tsx
+++ src/components/Buttons/LocationButton/index.tsx
@@ -1,9 +1,12 @@
 import Reac from "react";
 import { Container, Title } from "./styles";
+import { MapPin } from "phosphor-react-native";
+import theme from "@myApp/global/styles/theme";
 
 export function LocationButton(){
     return (
-        <Container>
+        <Container activeOpacity={.7}>
+            <MapPin color={theme.colors.purple} weight="fill" size={22}/>
             <Title>Maringá, PR</Title>
         </Container>
     )
--- src/components/Buttons/LocationButton/styles.ts
+++ src/components/Buttons/LocationButton/styles.ts
@@ -6,6 +6,8 @@ export const Container = styled.TouchableOpacity`
     border-radius: 6px;
     background-color: ${({ theme }) => theme.colors.purple_light};
     padding: 8px;
+    flex-direction: row;
+    align-items: center;
 
 `;
 
@@ -13,4 +15,5 @@ export const Title = styled.Text`
     font-size: ${RFValue(14)}px;
     color: ${ ({theme}) => theme.colors.purple_dark};
     font-family: ${({ theme }) => theme.fonts.roboto_regular};
+    margin-left: 4px;
 `;
