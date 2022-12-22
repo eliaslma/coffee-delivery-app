95dfe3bcfe5ce26976f6350e989b42969f71623d
Elias Lima <sci.eliaslima@gmail.com>
2022-12-21 22:17:36 -0300

    Added function to update total price of each selected item

--- src/screens/Checkout/index.tsx
+++ src/screens/Checkout/index.tsx
@@ -1,18 +1,45 @@
 import React from "react";
-import { useRoute } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { useState } from "react";
 import { FlatList } from "react-native";
 import { CoffeeSelected } from "@myApp/components/CoffeeSelected";
-import { Container, CartList, ListTitle, CartListContainer, Separator } from "./styles";
-
-
+import { CheckoutButton } from "@myApp/components/Buttons/CheckoutButton";
+import { 
+    Container, 
+    CartList, 
+    ListTitle, 
+    CartListContainer, 
+    Separator,
+    Footer,
+    TotalPriceContain,
+    TotalPrice,
+    TotalPriceItems
+} from "./styles";
 
 export function Checkout(){
-    const route = useRoute()
+
+    const route = useRoute();
+    const navigate = useNavigation();
+    const [totalCartPrice, setTotalPrice] = useState(route.params['totalCartPrice'])
     const [cartListSelected,setCartListSelected] = useState(route.params['data'])
-    
-    function handleRemoveCoffee(id: number){
-        setCartListSelected(oldState => oldState.filter(coffee => coffee.id !== id))
+    const totalCartPriceFormatted = Number(totalCartPrice).toLocaleString('pt-BR',{minimumFractionDigits: 2})
+        
+    function handleRemoveCoffee(id: number, totalPrice: number){
+        if(cartListSelected[1] == undefined){
+            navigate.goBack()
+        }
+        else {
+            handleDecrementPrice(totalPrice)
+            setCartListSelected(oldState => oldState.filter(coffee => coffee.id !== id))
+        }
+    }
+
+    function handleIncrementPrice(price: number){
+        setTotalPrice(totalCartPrice + price)
+    }
+
+    function handleDecrementPrice(price: number){
+        setTotalPrice(totalCartPrice - price)
     }
 
     return (
@@ -24,17 +51,34 @@ export function Checkout(){
                         data={cartListSelected}
                         showsVerticalScrollIndicator={false}
                         renderItem={ ({ item } ) => 
-                        <CoffeeSelected data={item} handleRemoveCoffee={handleRemoveCoffee} />}
+                        <CoffeeSelected 
+                            data={item} 
+                            handleRemoveCoffee={handleRemoveCoffee} 
+                            handleIncrementPrice={handleIncrementPrice}
+                            handleDecrementPrice={handleDecrementPrice}
+                        />}
                         ItemSeparatorComponent={ () =>
                             <Separator/>
                         }
                     />
                 </CartList>
             </CartListContainer>
+            <Footer>
+                <TotalPriceContain>
+                    <TotalPriceItems>Total de itens</TotalPriceItems>
+                    <TotalPriceItems>R$ {totalCartPriceFormatted}</TotalPriceItems>
+                </TotalPriceContain>
+                <TotalPriceContain>
+                    <TotalPriceItems>Entrega</TotalPriceItems>
+                    <TotalPriceItems>Grátis</TotalPriceItems>
+                </TotalPriceContain>
+                <TotalPriceContain>
+                    <TotalPrice>Total</TotalPrice>
+                    <TotalPrice>R$ {totalCartPriceFormatted}</TotalPrice>
+                </TotalPriceContain>
+                <CheckoutButton/>
+            </Footer>
         </Container>
 
     )
-        
-    
-    
 }
--- src/screens/Checkout/styles.ts
+++ src/screens/Checkout/styles.ts
@@ -1,9 +1,10 @@
-import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { getStatusBarHeight, getBottomSpace } from "react-native-iphone-x-helper";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
+import { Platform } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-
-export const Container = styled.View`
+export const Container = styled(GestureHandlerRootView)`
     flex: 1;
     background-color: ${({ theme }) => theme.colors.background};
 `;
@@ -19,19 +20,44 @@ export const ListTitle = styled.Text`
 
 export const CartListContainer = styled.View`
     flex: 1;
-    padding: 0 8px;
-    margin-top: 15px;
+    padding: 16px 8px;
 `;
 
 export const CartList = styled.View`
     background-color: ${({ theme }) => theme.colors.card};
-    border-radius: 6px;
-    border-bottom-left-radius: 30px;
-    border-top-right-radius: 30px;
+    border-radius: 8px;
+    
 `;
 
 export const Separator = styled.View`
     height: 1px;
     background-color: ${({theme}) => theme.colors.button};
-    margin: 12px 12px;
+    margin: 12px 24px;
+`;
+
+export const Footer = styled.View.attrs({
+    paddingBottom: Platform.OS === 'ios' ? getBottomSpace() + 16 : 16
+})`
+    padding-top: 16px;
+    background-color: ${({ theme }) => theme.colors.card};
+    border-top-width: 1px;
+    border-color: ${({theme}) => theme.colors.separator};
+`;
+
+export const TotalPriceContain = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    margin: 0px 16px 16px;
+`;
+
+export const TotalPrice = styled.Text`
+    font-family: ${({theme}) => theme.fonts.roboto_bold};;
+    font-size: ${RFValue(20)}px;
+    color: ${({theme}) => theme.colors.subtitle};
+`;
+
+export const TotalPriceItems = styled.Text`
+    font-family: ${({theme}) => theme.fonts.roboto_regular};;
+    font-size: ${RFValue(14)}px;
+    color: ${({theme}) => theme.colors.text};
 `;
