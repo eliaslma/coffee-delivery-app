d2e0a5129ff564556c8c95ad05907d701c44030f
Elias Lima <sci.eliaslima@gmail.com>
2022-12-21 22:04:39 -0300

    Created checkout button

---
+++ src/components/Buttons/CheckoutButton/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container, Title } from "./styles";
+
+export function CheckoutButton(){
+    return (
+        <Container>
+            <Title>CONFIRMAR PEDIDO</Title>
+        </Container>
+    )
+
+}
