296577609db38aba524c33db47a3e220e8e87091
Elias Lima <sci.eliaslima@gmail.com>
2022-12-24 23:24:00 -0300

    Added function to update location to home page

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from "react";
 import { Modal } from "react-native";
 import { FlatList } from "react-native";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
-import { getBottomSpace } from "react-native-iphone-x-helper";
+import { getBottomSpace, isIphoneX } from "react-native-iphone-x-helper";
 
 import { LocationButton } from "@myApp/components/Buttons/LocationButton";
 import { CoffeeCard } from "@myApp/components/CoffeeCard";
@@ -40,7 +40,9 @@ export function Home(){
     const navigation = useNavigation<PropsStack>();
     const [cartList, setCartList]  = useState<CardProps[]>([]);
     const [locationModalOpen,setLocationModalOpen] = useState(true);
-    const bottomSpace = getBottomSpace() // espaço da barra iphone x^
+    const [location, setLocation] = useState('');
+
+    
     
     function handleOpenLocationModal(){
         setLocationModalOpen(true);
@@ -48,6 +50,10 @@ export function Home(){
 
     function handleCloseLocationModal(){
         setLocationModalOpen(false);
+    }
+
+    function handleUpdateLocation(address: string){
+        setLocation(address)
     }
     
     function handleAddCart(id, quantity, price, name, photo){
@@ -96,13 +102,13 @@ export function Home(){
     },[]));
 
     return(
-        <>
+        
         <Container>     
             <Header>
                 <LocationWrapper>
                     <Logo source={require('../../assets/Logo-2.png')}/>    
                     <UserLocation>
-                        <LocationButton location={'Maringá, PR'} handlePress={handleOpenLocationModal}/>
+                        <LocationButton location={location} handlePress={handleOpenLocationModal}/>
                         <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/70176310?v=4'}}/>
                     </UserLocation>
                 </LocationWrapper>
@@ -128,18 +134,15 @@ export function Home(){
                         <CoffeeCard data={item} handlePressBuyButton={handleAddCart}/> }
                 />
                 {cartList.length != 0 && 
-                <Footer style={  bottomSpace ? {paddingBottom: bottomSpace} : {paddingBottom: 16}}>
+                <Footer style={  isIphoneX() ? {paddingBottom: getBottomSpace()} : {paddingBottom: 16}}>
                     <ViewCartButton quantity={totalQuantity} price={totalPrice} handlePress={handleNavCheckout}/>
                 </Footer>}
             </CoffeeList>
+            
+                <Modal visible={locationModalOpen} transparent={true} statusBarTranslucent={true}>
+                    <LocationModal handleCloseModal={handleCloseLocationModal} handleUpdateLocation={handleUpdateLocation}/>
+                </Modal>
         </Container>
-
-        <Modal visible={locationModalOpen} transparent={true}>
-            <LocationModal handlePress={handleCloseLocationModal}/>
-        </Modal>
-        
-        </>
-
     )
 }
 
