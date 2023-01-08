bf21f947c2615b67abf96a3bcfe7e06a85a2ca5e
Elias Lima <sci.eliaslima@gmail.com>
2023-01-08 17:08:23 -0300

    feat: Create authentication hook to consume google api

---
+++ src/hooks/Auth.tsx
@@ -0,0 +1,81 @@
+import React, { createContext, ReactNode, useContext, useState } from "react";
+import * as AuthSession from 'expo-auth-session';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+interface AuthProviderProps{
+    children: ReactNode;
+}
+
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    picture: string;
+}
+
+interface AuthContextProps{
+    user: User
+    SignInWithGoogle(): Promise<void>
+}
+
+type AuthResponseData = {
+    params:{
+        access_token: string;
+    }
+    type: string;
+}
+
+const AuthContext = createContext({} as AuthContextProps)
+
+function AuthProvider({ children } : AuthProviderProps){
+
+    const [userInfos, setUserInfos] = useState<User>({} as User)
+
+    async function SignInWithGoogle(){
+        try {
+            const CLIEND_ID = "ADICIONE SUA CLIENT_ID";
+            const REDIRECT_URI = "https://auth.expo.io/@eliaslma/coffee-delivery-app";
+            const SCOPE = encodeURI("profile email");
+            const RESPONSE_TYPE = "token";
+            const authUrl = 
+            `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIEND_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+            const {type, params} = await AuthSession.startAsync({authUrl}) as AuthResponseData;
+
+            if(type === 'success'){
+                const response = await axios(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`)
+                const userLogged: User = {
+                    id: response.data.id,
+                    name: response.data.name,
+                    email: response.data.email,
+                    picture: response.data.picture,
+                }
+                console.log('Chegay aqui',userLogged)
+                await AsyncStorage.setItem('@gofinances:user',JSON.stringify(userLogged))
+                setUserInfos(userLogged)
+            }
+
+        }catch(error){
+            throw Error(error)
+        }
+    
+    }
+
+    return(
+        <AuthContext.Provider 
+            value={{
+                user: userInfos,
+                SignInWithGoogle
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+function useAuth(){
+    const context = useContext(AuthContext)
+    return context;
+}
+
+export {AuthProvider, useAuth}
